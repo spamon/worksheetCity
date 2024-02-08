f80a667a45ed9d49b9b7aef8c5e889ef0223ea5a
spamon <ryan_mcgrath@hotmail.co.uk>
2024-02-08 12:31:06 +0000

    Update worksheetCity.py

--- worksheetCity.py
+++ worksheetCity.py
@@ -146,6 +146,10 @@ def extract_vertical_blind_data(driver, customer_name):
             product_data = calculate_sizes_day_and_night_blinds(product_data)
         elif product_data.get('Product Type') == 'Perfect Fit Pleated':
             product_data = calculate_sizes_perfect_fit_pleated(product_data)
+        elif product_data.get('Product Type') == 'Roman Blind':
+            product_data = calculate_sizes_roman_blinds(product_data)
+        if 'Height required' in product_data:
+            product_data = calculate_sizes_vertical_blind_slats(product_data)
         
 
         all_product_data.append(product_data)
@@ -180,40 +184,55 @@ def calculate_sizes_allusion_blinds(product_data):
             if louver_drop_size is not None:
                 louver_drop_size += 10
 
-        # For Allusion Blinds, subtract 12mm from the length
-        length_str = product_data.get('Length', '0')
-        length = convert_to_mm(length_str)
-        if length is not None:
-            adjusted_length = length - 12
-            louver_drop_size = adjusted_length  # Store the adjusted louver drop size in a variable
-            product_data['Length'] = f'{adjusted_length}mm'  # Update the Length value
-        else:
-            louver_drop_size = None
-            product_data['Louver Drop Size'] = 'Invalid Length'
+def calculate_sizes_allusion_blinds(product_data):
+    # Filter only for Allusion Blinds
+    if product_data.get('Product Type') == 'Allusion Blind':
+        width_str = product_data.get('Width', '0')
+        width = convert_to_mm(width_str)
 
-        # Check if louver drop size is not None and add it to product_data
-        if louver_drop_size is not None:
-            product_data['Louver Drop Size'] = f'{louver_drop_size}mm'
+        length_str = product_data.get('Length', '0')  # Initialize length_str
+        length = convert_to_mm(length_str)  # Initialize length
+
+        louver_drop_size = None  # Initialize louver_drop_size
+
+        if width is None or length is None:
+            product_data['Finished Size'] = 'Invalid Width' if width is None else ''
+            product_data['Cut Rail Size'] = 'Invalid Width' if width is None else ''
+            product_data['Louver Drop Size'] = 'Invalid Length' if length is None else ''
+            product_data['Qty Louvers'] = ''
+            return product_data
+
+        operation_type = product_data.get('Operation Types')
+        measurement_type = product_data.get('Measurement Type')
+
+        finished_size = cut_rail_size = 0
+
+        if measurement_type == 'Recess':
+            finished_size = width - 10
+        elif measurement_type == 'Exact':
+            finished_size = width
 
         if operation_type == 'Manual Operation':
             cut_rail_size = finished_size - 20
-        elif operation_type == 'Want Operation':
+            product_data['Child Safety'] = 'Side Fix'  # Example value, adjust as needed
+        elif operation_type == 'Wand Operation':
             cut_rail_size = finished_size - 10
+            product_data['Child Safety'] = 'Wand Control'  # Example value, adjust as needed
 
         product_data['Finished Size'] = f'{finished_size}mm'
         product_data['Cut Rail Size'] = f'{cut_rail_size}mm'
         product_data['Qty Louvers'] = ''
-        
-        # Check if louver drop size is not None and add it to product_data
-        if louver_drop_size is not None:
+
+        # Update louver drop size and length
+        if length is not None:
+            louver_drop_size = length - 12  # Adjusting louver drop size
             product_data['Louver Drop Size'] = f'{louver_drop_size}mm'
+            product_data['Length'] = f'{length}mm'
         else:
             product_data['Louver Drop Size'] = 'Invalid Length'
 
-        # Update the Length value for Allusion Blinds
-        product_data['Length'] = f'{length}mm'
-
     return product_data
+
 
 
 
@@ -398,6 +417,56 @@ def calculate_sizes_perfect_fit_pleated(product_data):
 
     return product_data
 
+def calculate_sizes_roman_blinds(product_data):
+    if product_data.get('Product Type') == 'Roman Blind':
+        width_str = product_data.get('Width', '0')
+        width = convert_to_mm(width_str)
+
+        if width is None:
+            product_data['Finished Width'] = 'Invalid Width'
+            return product_data
+
+        measurement_type = product_data.get('Measurement Type')
+
+        finished_width = width
+
+        if measurement_type == 'Recess':
+            finished_width -= 10  # Subtract 10mm for recess measurement
+
+        # Convert drop to mm but don't change the value
+        drop_str = product_data.get('Length', '0')
+        length = convert_to_mm(drop_str)
+        if length is None:
+            product_data['Finished Drop'] = 'Invalid Drop'
+        else:
+            product_data['Finished Length'] = f'{length}mm'
+
+        product_data['Finished Width'] = f'{finished_width}mm'
+        
+        # Remove unwanted keys
+        product_data.pop('Width', None)
+        product_data.pop('Length', None)
+        product_data.pop('Measurement Protection', None)
+
+    return product_data
+
+def calculate_sizes_vertical_blind_slats(product_data):
+    height_str = product_data.get('Height required', '0')
+    height = convert_to_mm(height_str)
+
+    if height is None:
+        product_data['Adjusted Height'] = 'Invalid Height'
+    else:
+        adjusted_height = height + 50  # Add 50mm to the height
+        product_data['Adjusted Height'] = f'{adjusted_height}mm'
+
+    # # Remove unwanted keys
+    product_data.pop('Height required', None)
+    # product_data.pop('Replacement Vertical Slat Weights & Chains required', None)
+
+    return product_data
+
+
 
 
 
@@ -421,10 +490,10 @@ def main():
 
     # List of URLs to process
     order_urls = [
-        "https://www.emeraldblindsandcurtains.co.uk/z-admin/orders/view/4139/",
-        # "https://www.emeraldblindsandcurtains.co.uk/z-admin/orders/view/4134/",
-        # "https://www.emeraldblindsandcurtains.co.uk/z-admin/orders/view/4133/",
-        # "https://www.emeraldblindsandcurtains.co.uk/z-admin/orders/view/4132/",
+        "https://www.emeraldblindsandcurtains.co.uk/z-admin/orders/view/4169/",
+        "https://www.emeraldblindsandcurtains.co.uk/z-admin/orders/view/4134/",
+        "https://www.emeraldblindsandcurtains.co.uk/z-admin/orders/view/4159/",
+        "https://www.emeraldblindsandcurtains.co.uk/z-admin/orders/view/4102/",
         # "https://www.emeraldblindsandcurtains.co.uk/z-admin/orders/view/4131/"
         # Add more URLs here
     ]
